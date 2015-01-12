d34814a11ead4bde36542de350997d1e1115ebf3
gitester1003 <docker_site3@yahoo.com>
2015-01-13 00:29:00 +0800

    Update hello.rb
    edit to 13

--- hello.rb
+++ hello.rb
@@ -1,2 +1,11 @@
 #!/usr/bin/env ruby
 print "Hello, World!\n"
+a = 0
+while a < 15
+    print a, " "
+    if a == 13 then
+        print "(a = thirteen)\n"
+    end
+    a = a + 1
+end
+print "\n\n"
